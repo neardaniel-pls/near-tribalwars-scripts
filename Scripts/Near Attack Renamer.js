921b7b85873a3167a265543501388ebc7c7c0c91
neardaniel-pls <daniel_rodrigues@tutanota.com>
2021-04-29 14:20:03 +0100

    New comments

--- Scripts/Near Attack Renamer.js
+++ Scripts/Near Attack Renamer.js
@@ -3,17 +3,26 @@
 // @description Every 20 seconds, all attacks get selected and the "Rename" button gets clicked
 // @author Near
 // @include https://*&mode=incomings*
-// @version 1.0.1
+// @version 1.0.2
 // @namespace https://greasyfork.org/users/471382
 // ==/UserScript==
 
 setTimeout(function() {
     location.reload(1);
 }, 20000); {
+
+    //clicks the select all button
     $('input#select_all.selectAll').click();
+
+    //starts setTimeout funcion of 20000ms
     setTimeout(function() {
+
+        //searches for the label button
         var label = document.getElementsByName("label");
+
+        //clicks on the label button
         label[0].click();
+
         //refreshes the page every 20 seconds aka 20000ms
     }, 20000);
 }
